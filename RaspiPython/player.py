8db1c273b6c95c38fcd6c55090da7e3bc6d797df
Devon Peet <bearbiever@gmail.com>
2017-03-31 19:22:24 -0400

    update player

--- RaspiPython/player.py
+++ RaspiPython/player.py
@@ -1,8 +1,8 @@
 class Player:
     def __init__(self, *args, **kwargs):
-##    def __init__(self, name):
-##        print(kwargs)
-##        print(args[0])
+        ##    def __init__(self, name):
+        ##        print(kwargs)
+        ##        print(args[0])
         self.name = args[0]["name"]
         self.clss = args[0]["class"]
         self.background = args[0]["background"]
@@ -22,5 +22,10 @@ class Player:
         self.cha = args[0]["cha"]
 
         ## Equipments
-        self.armor = args[0]["armor"]
+        self.armorclass = args[0]["armor_class"]
         self.speed = args[0]["speed"]
+
+        ##
+        self.weapons = args[0]["weapons"]
+        self.spells = args[0]["spells"]
+        self.special = args[0]["special"]
